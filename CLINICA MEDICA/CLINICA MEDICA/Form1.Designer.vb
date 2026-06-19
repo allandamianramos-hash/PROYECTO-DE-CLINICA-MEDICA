27f8bd904a1aec8e7b8080a144517ae0a4b1c0ea
Justin <justinavila@franciscojavier.edu.hn>
2026-06-18 22:38:27 -0600

    Hice un boton de limpieza

--- CLINICA MEDICA/CLINICA MEDICA/Form1.Designer.vb
+++ CLINICA MEDICA/CLINICA MEDICA/Form1.Designer.vb
@@ -24,13 +24,16 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Button1 = New Button()
         Label1 = New Label()
+        btn2 = New Button()
+        txb1 = New TextBox()
         SuspendLayout()
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(318, 194)
+        Button1.Location = New Point(278, 146)
+        Button1.Margin = New Padding(3, 2, 3, 2)
         Button1.Name = "Button1"
-        Button1.Size = New Size(94, 29)
+        Button1.Size = New Size(82, 22)
         Button1.TabIndex = 0
         Button1.Text = "Salir"
         Button1.UseVisualStyleBackColor = True
@@ -39,19 +42,38 @@ Partial Class Form1
         ' 
         Label1.AutoSize = True
         Label1.Font = New Font("Showcard Gothic", 12F, FontStyle.Italic, GraphicsUnit.Point, CByte(0))
-        Label1.Location = New Point(313, 35)
+        Label1.Location = New Point(274, 26)
         Label1.Name = "Label1"
-        Label1.Size = New Size(99, 26)
+        Label1.Size = New Size(80, 20)
         Label1.TabIndex = 1
         Label1.Text = "EJEMPLO"
         ' 
+        ' btn2
+        ' 
+        btn2.Location = New Point(518, 63)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(75, 23)
+        btn2.TabIndex = 2
+        btn2.Text = "clean"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' txb1
+        ' 
+        txb1.Location = New Point(504, 110)
+        txb1.Name = "txb1"
+        txb1.Size = New Size(100, 23)
+        txb1.TabIndex = 3
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(700, 338)
+        Controls.Add(txb1)
+        Controls.Add(btn2)
         Controls.Add(Label1)
         Controls.Add(Button1)
+        Margin = New Padding(3, 2, 3, 2)
         Name = "Form1"
         Text = "Ejemplo"
         ResumeLayout(False)
@@ -60,5 +82,7 @@ Partial Class Form1
 
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents btn2 As Button
+    Friend WithEvents txb1 As TextBox
 
 End Class
--- CLINICA MEDICA/CLINICA MEDICA/Form1.vb
+++ CLINICA MEDICA/CLINICA MEDICA/Form1.vb
@@ -1,5 +1,11 @@
-﻿Public Class Form1
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.Hide()
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        txb1.Clear()
     End Sub
 End Class
