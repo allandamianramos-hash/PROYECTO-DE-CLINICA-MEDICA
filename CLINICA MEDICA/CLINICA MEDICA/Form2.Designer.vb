312e32756ac473e46d6b349841d5a2c973e3d49b
Din-pnj <dilanbanegas@franciscojavier.edu.hn>
2026-06-22 21:37:54 -0600

    Hice el segundo formulario.

---
+++ CLINICA MEDICA/CLINICA MEDICA/Form2.Designer.vb
@@ -0,0 +1,299 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmPacientes
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtIdPaciente = New TextBox()
+        txtNombre = New TextBox()
+        txtApellido = New TextBox()
+        txtDireccion = New TextBox()
+        txtTelefono = New TextBox()
+        txtCorreo = New TextBox()
+        txtBuscar = New TextBox()
+        dtpFechaNac = New DateTimePicker()
+        cmbSexo = New ComboBox()
+        dgvPacientes = New DataGridView()
+        btnNuevo = New Button()
+        btnGuardar = New Button()
+        btnEditar = New Button()
+        btnEliminar = New Button()
+        btnLimpiar = New Button()
+        btnPrimero = New Button()
+        btnAnterior = New Button()
+        btnSiguiente = New Button()
+        btnUltimo = New Button()
+        btnRegresar = New Button()
+        btnSalir = New Button()
+        CType(dgvPacientes, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Showcard Gothic", 12F, FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(359, 18)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(189, 20)
+        Label1.TabIndex = 2
+        Label1.Text = "GESTIÓN DE PACIENTES"
+        ' 
+        ' txtIdPaciente
+        ' 
+        txtIdPaciente.Location = New Point(115, 56)
+        txtIdPaciente.Name = "txtIdPaciente"
+        txtIdPaciente.ReadOnly = True
+        txtIdPaciente.Size = New Size(100, 23)
+        txtIdPaciente.TabIndex = 3
+        txtIdPaciente.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtNombre
+        ' 
+        txtNombre.Location = New Point(115, 85)
+        txtNombre.Name = "txtNombre"
+        txtNombre.Size = New Size(100, 23)
+        txtNombre.TabIndex = 4
+        txtNombre.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtApellido
+        ' 
+        txtApellido.Location = New Point(115, 114)
+        txtApellido.Name = "txtApellido"
+        txtApellido.Size = New Size(100, 23)
+        txtApellido.TabIndex = 5
+        txtApellido.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtDireccion
+        ' 
+        txtDireccion.Location = New Point(115, 201)
+        txtDireccion.Multiline = True
+        txtDireccion.Name = "txtDireccion"
+        txtDireccion.Size = New Size(100, 23)
+        txtDireccion.TabIndex = 6
+        txtDireccion.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtTelefono
+        ' 
+        txtTelefono.Location = New Point(115, 230)
+        txtTelefono.Name = "txtTelefono"
+        txtTelefono.Size = New Size(100, 23)
+        txtTelefono.TabIndex = 7
+        txtTelefono.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtCorreo
+        ' 
+        txtCorreo.Location = New Point(115, 259)
+        txtCorreo.Name = "txtCorreo"
+        txtCorreo.Size = New Size(100, 23)
+        txtCorreo.TabIndex = 8
+        txtCorreo.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtBuscar
+        ' 
+        txtBuscar.Location = New Point(115, 288)
+        txtBuscar.Name = "txtBuscar"
+        txtBuscar.Size = New Size(100, 23)
+        txtBuscar.TabIndex = 9
+        txtBuscar.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' dtpFechaNac
+        ' 
+        dtpFechaNac.Format = DateTimePickerFormat.Short
+        dtpFechaNac.Location = New Point(115, 143)
+        dtpFechaNac.Name = "dtpFechaNac"
+        dtpFechaNac.Size = New Size(100, 23)
+        dtpFechaNac.TabIndex = 10
+        ' 
+        ' cmbSexo
+        ' 
+        cmbSexo.FormattingEnabled = True
+        cmbSexo.Items.AddRange(New Object() {"M", "F"})
+        cmbSexo.Location = New Point(115, 172)
+        cmbSexo.Name = "cmbSexo"
+        cmbSexo.Size = New Size(100, 23)
+        cmbSexo.TabIndex = 11
+        ' 
+        ' dgvPacientes
+        ' 
+        dgvPacientes.AllowUserToAddRows = False
+        dgvPacientes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvPacientes.Location = New Point(221, 56)
+        dgvPacientes.Name = "dgvPacientes"
+        dgvPacientes.Size = New Size(667, 255)
+        dgvPacientes.TabIndex = 12
+        ' 
+        ' btnNuevo
+        ' 
+        btnNuevo.Location = New Point(12, 338)
+        btnNuevo.Name = "btnNuevo"
+        btnNuevo.Size = New Size(75, 23)
+        btnNuevo.TabIndex = 13
+        btnNuevo.Text = "Nuevo"
+        btnNuevo.UseVisualStyleBackColor = True
+        ' 
+        ' btnGuardar
+        ' 
+        btnGuardar.Location = New Point(212, 338)
+        btnGuardar.Name = "btnGuardar"
+        btnGuardar.Size = New Size(75, 23)
+        btnGuardar.TabIndex = 14
+        btnGuardar.Text = "Guardar"
+        btnGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' btnEditar
+        ' 
+        btnEditar.Location = New Point(426, 338)
+        btnEditar.Name = "btnEditar"
+        btnEditar.Size = New Size(75, 23)
+        btnEditar.TabIndex = 15
+        btnEditar.Text = "Editar"
+        btnEditar.UseVisualStyleBackColor = True
+        ' 
+        ' btnEliminar
+        ' 
+        btnEliminar.Location = New Point(628, 338)
+        btnEliminar.Name = "btnEliminar"
+        btnEliminar.Size = New Size(75, 23)
+        btnEliminar.TabIndex = 16
+        btnEliminar.Text = "Eliminar"
+        btnEliminar.UseVisualStyleBackColor = True
+        ' 
+        ' btnLimpiar
+        ' 
+        btnLimpiar.Location = New Point(813, 338)
+        btnLimpiar.Name = "btnLimpiar"
+        btnLimpiar.Size = New Size(75, 23)
+        btnLimpiar.TabIndex = 17
+        btnLimpiar.Text = "Limpiar"
+        btnLimpiar.UseVisualStyleBackColor = True
+        ' 
+        ' btnPrimero
+        ' 
+        btnPrimero.Location = New Point(570, 476)
+        btnPrimero.Name = "btnPrimero"
+        btnPrimero.Size = New Size(75, 23)
+        btnPrimero.TabIndex = 18
+        btnPrimero.Text = "<<"
+        btnPrimero.UseVisualStyleBackColor = True
+        ' 
+        ' btnAnterior
+        ' 
+        btnAnterior.Location = New Point(651, 476)
+        btnAnterior.Name = "btnAnterior"
+        btnAnterior.Size = New Size(75, 23)
+        btnAnterior.TabIndex = 19
+        btnAnterior.Text = "<"
+        btnAnterior.UseVisualStyleBackColor = True
+        ' 
+        ' btnSiguiente
+        ' 
+        btnSiguiente.Location = New Point(732, 476)
+        btnSiguiente.Name = "btnSiguiente"
+        btnSiguiente.Size = New Size(75, 23)
+        btnSiguiente.TabIndex = 20
+        btnSiguiente.Text = ">"
+        btnSiguiente.UseVisualStyleBackColor = True
+        ' 
+        ' btnUltimo
+        ' 
+        btnUltimo.Location = New Point(813, 476)
+        btnUltimo.Name = "btnUltimo"
+        btnUltimo.Size = New Size(75, 23)
+        btnUltimo.TabIndex = 21
+        btnUltimo.Text = ">>"
+        btnUltimo.UseVisualStyleBackColor = True
+        ' 
+        ' btnRegresar
+        ' 
+        btnRegresar.Location = New Point(197, 413)
+        btnRegresar.Name = "btnRegresar"
+        btnRegresar.Size = New Size(109, 23)
+        btnRegresar.TabIndex = 22
+        btnRegresar.Text = "Menú principal"
+        btnRegresar.UseVisualStyleBackColor = True
+        ' 
+        ' btnSalir
+        ' 
+        btnSalir.Location = New Point(628, 413)
+        btnSalir.Name = "btnSalir"
+        btnSalir.Size = New Size(75, 23)
+        btnSalir.TabIndex = 23
+        btnSalir.Text = "Salir"
+        btnSalir.UseVisualStyleBackColor = True
+        ' 
+        ' frmPacientes
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(900, 511)
+        Controls.Add(btnSalir)
+        Controls.Add(btnRegresar)
+        Controls.Add(btnUltimo)
+        Controls.Add(btnSiguiente)
+        Controls.Add(btnAnterior)
+        Controls.Add(btnPrimero)
+        Controls.Add(btnLimpiar)
+        Controls.Add(btnEliminar)
+        Controls.Add(btnEditar)
+        Controls.Add(btnGuardar)
+        Controls.Add(btnNuevo)
+        Controls.Add(dgvPacientes)
+        Controls.Add(cmbSexo)
+        Controls.Add(dtpFechaNac)
+        Controls.Add(txtBuscar)
+        Controls.Add(txtCorreo)
+        Controls.Add(txtTelefono)
+        Controls.Add(txtDireccion)
+        Controls.Add(txtApellido)
+        Controls.Add(txtNombre)
+        Controls.Add(txtIdPaciente)
+        Controls.Add(Label1)
+        Name = "frmPacientes"
+        Text = "Form2"
+        CType(dgvPacientes, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtIdPaciente As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtDireccion As TextBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents txtCorreo As TextBox
+    Friend WithEvents txtBuscar As TextBox
+    Friend WithEvents dtpFechaNac As DateTimePicker
+    Friend WithEvents cmbSexo As ComboBox
+    Friend WithEvents dgvPacientes As DataGridView
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnEditar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnPrimero As Button
+    Friend WithEvents btnAnterior As Button
+    Friend WithEvents btnSiguiente As Button
+    Friend WithEvents btnUltimo As Button
+    Friend WithEvents btnRegresar As Button
+    Friend WithEvents btnSalir As Button
+End Class
---
+++ CLINICA MEDICA/CLINICA MEDICA/Form2.vb
@@ -0,0 +1,255 @@
+﻿'Clase del formulario de pacientes
+Public Class frmPacientes
+
+    'Variable que permitirá controlar la posición actual
+    'cuando se implemente la navegación de registros.
+    Private indiceActual As Integer = 0
+    Private Sub frmPacientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Limpiar posibles elementos existentes.
+        cmbSexo.Items.Clear()
+
+        'Agregar las opciones permitidas según la tabla.
+        cmbSexo.Items.Add("M")
+        cmbSexo.Items.Add("F")
+
+        'El ID se genera automáticamente en PostgreSQL,
+        'por lo tanto no debe ser editable.
+        txtIdPaciente.ReadOnly = True
+
+        'Evita que el usuario agregue filas manualmente.
+        dgvPacientes.AllowUserToAddRows = False
+
+    End Sub
+    Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+
+        'Limpia todos los controles del formulario.
+        LimpiarCampos()
+
+        'Coloca el cursor en el campo Nombre.
+        txtNombre.Focus()
+
+    End Sub
+    Private Sub LimpiarCampos()
+
+        'Vacía todas las cajas de texto.
+        txtIdPaciente.Clear()
+        txtNombre.Clear()
+        txtApellido.Clear()
+        txtDireccion.Clear()
+        txtTelefono.Clear()
+        txtCorreo.Clear()
+        txtBuscar.Clear()
+
+        'Quita la selección actual de la caja de combo.
+        cmbSexo.SelectedIndex = -1
+
+        'Restablece la fecha al día actual.
+        dtpFechaNac.Value = Date.Today
+
+    End Sub
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+
+        'Llama al método encargado de limpiar controles.
+        LimpiarCampos()
+
+    End Sub
+    Private Function ValidarCampos() As Boolean
+
+        'Validar nombre.
+        If txtNombre.Text.Trim = "" Then
+
+            MessageBox.Show("Debe ingresar el nombre.")
+
+            txtNombre.Focus()
+
+            Return False
+
+        End If
+
+        'Validar apellido.
+        If txtApellido.Text.Trim = "" Then
+
+            MessageBox.Show("Debe ingresar el apellido.")
+
+            txtApellido.Focus()
+
+            Return False
+
+        End If
+
+        'Validar sexo.
+        If cmbSexo.SelectedIndex = -1 Then
+
+            MessageBox.Show("Debe seleccionar el sexo.")
+
+            cmbSexo.Focus()
+
+            Return False
+
+        End If
+
+        'Validar teléfono.
+        If txtTelefono.Text.Trim = "" Then
+
+            MessageBox.Show("Debe ingresar el teléfono.")
+
+            txtTelefono.Focus()
+
+            Return False
+
+        End If
+
+        'Validar correo.
+        If txtCorreo.Text.Trim = "" Then
+
+            MessageBox.Show("Debe ingresar el correo electrónico.")
+
+            txtCorreo.Focus()
+
+            Return False
+
+        End If
+
+        'Validación simple del formato del correo.
+        If Not txtCorreo.Text.Contains("@") Then
+
+            MessageBox.Show("Correo electrónico inválido.")
+
+            txtCorreo.Focus()
+
+            Return False
+
+        End If
+
+        'Si todas las validaciones son correctas.
+        Return True
+
+    End Function
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+
+        'Ejecutar validaciones.
+        If Not ValidarCampos() Then Exit Sub
+
+        'Mensaje temporal.
+        MessageBox.Show("Paciente guardado correctamente.")
+
+        'Aquí posteriormente se llamará al procedimiento:
+        '
+        'CALL registrar_paciente(...)
+        '
+        'utilizando Npgsql.
+
+    End Sub
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+
+        'Verificar que exista un registro seleccionado.
+        If txtIdPaciente.Text = "" Then
+
+            MessageBox.Show("Seleccione un registro.")
+
+            Exit Sub
+
+        End If
+
+        'Validar los datos.
+        If Not ValidarCampos() Then Exit Sub
+
+        'Mensaje temporal.
+        MessageBox.Show("Paciente actualizado correctamente.")
+
+        'Aquí posteriormente se llamará:
+        '
+        'CALL actualizar_paciente(...)
+
+    End Sub
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+
+        'Verificar selección.
+        If txtIdPaciente.Text = "" Then
+
+            MessageBox.Show("Seleccione un paciente.")
+
+            Exit Sub
+
+        End If
+
+        'Solicitar confirmación al usuario.
+        If MessageBox.Show(
+        "¿Desea eliminar este paciente?",
+        "Confirmar",
+        MessageBoxButtons.YesNo,
+        MessageBoxIcon.Question) = DialogResult.Yes Then
+
+            MessageBox.Show("Paciente eliminado.")
+
+            'Posteriormente:
+            '
+            'CALL eliminar_paciente(...)
+
+        End If
+
+    End Sub
+    Private Sub txtBuscar_TextChanged(sender As Object, e As EventArgs) Handles txtBuscar.TextChanged
+
+        'Cada vez que el usuario escriba,
+        'este evento se ejecutará.
+
+        'Posteriormente permitirá buscar
+        'pacientes por nombre o apellido
+        'en PostgreSQL.
+
+    End Sub
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+
+        'Cerrar completamente la aplicación.
+        Application.Exit()
+
+    End Sub
+    Private Sub btnRegresar_Click(sender As Object, e As EventArgs) Handles btnRegresar.Click
+
+        'Cerrar únicamente este formulario
+        'y regresar al formulario anterior.
+        Me.Close()
+
+    End Sub
+    Private Sub txtNombre_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNombre.KeyPress
+
+        'Permitir únicamente letras,
+        'espacios y teclas de control.
+        If Not Char.IsLetter(e.KeyChar) And
+           Not Char.IsControl(e.KeyChar) And
+           e.KeyChar <> " "c Then
+
+            'Bloquear el carácter.
+            e.Handled = True
+
+        End If
+
+    End Sub
+    Private Sub txtApellido_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtApellido.KeyPress
+
+        'Permitir únicamente letras,
+        'espacios y teclas de control.
+        If Not Char.IsLetter(e.KeyChar) And
+           Not Char.IsControl(e.KeyChar) And
+           e.KeyChar <> " "c Then
+
+            e.Handled = True
+
+        End If
+
+    End Sub
+    Private Sub txtTelefono_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtTelefono.KeyPress
+
+        'Permitir únicamente números.
+        If Not Char.IsDigit(e.KeyChar) And
+           Not Char.IsControl(e.KeyChar) Then
+
+            'Bloquear cualquier letra o símbolo.
+            e.Handled = True
+
+        End If
+
+    End Sub
+End Class
