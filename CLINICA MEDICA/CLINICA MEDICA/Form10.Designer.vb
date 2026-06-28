923fe7a0055fd90e21023bee7bb89ea7295dae23
Allan Ramos <allandamianramos@gmail.com>
2026-06-27 22:28:56 -0600

    Edite unos cambios de mi companero en el form10 (facturas)

--- CLINICA MEDICA/CLINICA MEDICA/Form10.Designer.vb
+++ CLINICA MEDICA/CLINICA MEDICA/Form10.Designer.vb
@@ -55,263 +55,264 @@ Partial Class Form10
         ' 
         ' txtIdFactura
         ' 
-        txtIdFactura.Location = New Point(14, 45)
-        txtIdFactura.Margin = New Padding(3, 2, 3, 2)
+        txtIdFactura.Location = New Point(16, 60)
         txtIdFactura.Name = "txtIdFactura"
         txtIdFactura.ReadOnly = True
-        txtIdFactura.Size = New Size(102, 23)
+        txtIdFactura.Size = New Size(116, 27)
         txtIdFactura.TabIndex = 0
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(14, 28)
+        Label1.Location = New Point(16, 37)
         Label1.Name = "Label1"
-        Label1.Size = New Size(59, 15)
+        Label1.Size = New Size(74, 20)
         Label1.TabIndex = 1
         Label1.Text = "Id_factura"
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(14, 78)
+        Label2.Location = New Point(16, 104)
         Label2.Name = "Label2"
-        Label2.Size = New Size(54, 15)
+        Label2.Size = New Size(66, 20)
         Label2.TabIndex = 2
         Label2.Text = "Consulta"
         ' 
         ' cmbConsulta
         ' 
         cmbConsulta.FormattingEnabled = True
-        cmbConsulta.Location = New Point(14, 95)
-        cmbConsulta.Margin = New Padding(3, 2, 3, 2)
+        cmbConsulta.Location = New Point(16, 127)
         cmbConsulta.Name = "cmbConsulta"
-        cmbConsulta.Size = New Size(102, 23)
+        cmbConsulta.Size = New Size(116, 28)
         cmbConsulta.TabIndex = 4
         ' 
         ' dtpFechaPago
         ' 
         dtpFechaPago.CustomFormat = "dd/MM/yyyy - HH:mm:ss"
         dtpFechaPago.Format = DateTimePickerFormat.Custom
-        dtpFechaPago.Location = New Point(130, 183)
-        dtpFechaPago.Margin = New Padding(3, 2, 3, 2)
+        dtpFechaPago.Location = New Point(149, 244)
         dtpFechaPago.Name = "dtpFechaPago"
-        dtpFechaPago.Size = New Size(166, 23)
+        dtpFechaPago.Size = New Size(189, 27)
         dtpFechaPago.TabIndex = 5
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(171, 165)
+        Label3.Location = New Point(195, 220)
         Label3.Name = "Label3"
-        Label3.Size = New Size(84, 15)
+        Label3.Size = New Size(107, 20)
         Label3.TabIndex = 6
         Label3.Text = "Fecha de pago"
         ' 
         ' cmbMetodoPago
         ' 
         cmbMetodoPago.FormattingEnabled = True
-        cmbMetodoPago.Location = New Point(14, 154)
-        cmbMetodoPago.Margin = New Padding(3, 2, 3, 2)
+        cmbMetodoPago.Location = New Point(16, 205)
         cmbMetodoPago.Name = "cmbMetodoPago"
-        cmbMetodoPago.Size = New Size(102, 23)
+        cmbMetodoPago.Size = New Size(116, 28)
         cmbMetodoPago.TabIndex = 7
         ' 
         ' Label4
         ' 
         Label4.AutoSize = True
-        Label4.Location = New Point(14, 137)
+        Label4.Location = New Point(16, 183)
         Label4.Name = "Label4"
-        Label4.Size = New Size(95, 15)
+        Label4.Size = New Size(120, 20)
         Label4.TabIndex = 8
         Label4.Text = "Metodo de Pago"
         ' 
         ' Label5
         ' 
         Label5.AutoSize = True
-        Label5.Location = New Point(14, 188)
+        Label5.Location = New Point(16, 251)
         Label5.Name = "Label5"
-        Label5.Size = New Size(88, 15)
+        Label5.Size = New Size(112, 20)
         Label5.TabIndex = 9
         Label5.Text = "Estado de Pago"
         ' 
         ' cmbEstadoPago
         ' 
         cmbEstadoPago.FormattingEnabled = True
-        cmbEstadoPago.Location = New Point(14, 205)
-        cmbEstadoPago.Margin = New Padding(3, 2, 3, 2)
+        cmbEstadoPago.Location = New Point(16, 273)
         cmbEstadoPago.Name = "cmbEstadoPago"
-        cmbEstadoPago.Size = New Size(102, 23)
+        cmbEstadoPago.Size = New Size(116, 28)
         cmbEstadoPago.TabIndex = 10
         ' 
         ' txtMontoTotal
         ' 
-        txtMontoTotal.Location = New Point(14, 261)
-        txtMontoTotal.Margin = New Padding(3, 2, 3, 2)
+        txtMontoTotal.Location = New Point(16, 348)
         txtMontoTotal.Name = "txtMontoTotal"
         txtMontoTotal.ReadOnly = True
-        txtMontoTotal.Size = New Size(110, 23)
+        txtMontoTotal.Size = New Size(125, 27)
         txtMontoTotal.TabIndex = 12
         ' 
         ' Label6
         ' 
         Label6.AutoSize = True
-        Label6.Location = New Point(14, 244)
+        Label6.Location = New Point(16, 325)
         Label6.Name = "Label6"
-        Label6.Size = New Size(70, 15)
+        Label6.Size = New Size(88, 20)
         Label6.TabIndex = 13
         Label6.Text = "Monto total"
         ' 
         ' dgvFacturas
         ' 
         dgvFacturas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        dgvFacturas.Location = New Point(351, 47)
-        dgvFacturas.Margin = New Padding(3, 2, 3, 2)
+        dgvFacturas.Location = New Point(401, 63)
         dgvFacturas.Name = "dgvFacturas"
         dgvFacturas.RowHeadersWidth = 51
-        dgvFacturas.Size = New Size(725, 274)
+        dgvFacturas.Size = New Size(829, 365)
         dgvFacturas.TabIndex = 14
         ' 
         ' clbMedicamentos
         ' 
         clbMedicamentos.FormattingEnabled = True
-        clbMedicamentos.Location = New Point(145, 47)
-        clbMedicamentos.Margin = New Padding(3, 2, 3, 2)
+        clbMedicamentos.Location = New Point(166, 63)
         clbMedicamentos.Name = "clbMedicamentos"
-        clbMedicamentos.Size = New Size(143, 40)
+        clbMedicamentos.Size = New Size(163, 48)
         clbMedicamentos.TabIndex = 15
         ' 
         ' Label7
         ' 
         Label7.AutoSize = True
         Label7.Font = New Font("Lucida Sans Unicode", 9.75F, FontStyle.Bold)
-        Label7.Location = New Point(402, 7)
+        Label7.Location = New Point(459, 9)
         Label7.Name = "Label7"
-        Label7.Size = New Size(146, 16)
+        Label7.Size = New Size(188, 21)
         Label7.TabIndex = 16
         Label7.Text = "Módulo de facturas"
         ' 
         ' Label8
         ' 
         Label8.AutoSize = True
-        Label8.Location = New Point(143, 26)
+        Label8.Location = New Point(163, 35)
         Label8.Name = "Label8"
-        Label8.Size = New Size(145, 15)
+        Label8.Size = New Size(182, 20)
         Label8.TabIndex = 17
         Label8.Text = "Medicamentos adquiridos"
         ' 
         ' btnUltimo
         ' 
-        btnUltimo.Location = New Point(529, 363)
+        btnUltimo.Location = New Point(605, 484)
+        btnUltimo.Margin = New Padding(3, 4, 3, 4)
         btnUltimo.Name = "btnUltimo"
-        btnUltimo.Size = New Size(69, 25)
+        btnUltimo.Size = New Size(79, 33)
         btnUltimo.TabIndex = 54
         btnUltimo.Text = ">>"
         btnUltimo.UseVisualStyleBackColor = True
         ' 
         ' btnSiguiente
         ' 
-        btnSiguiente.Location = New Point(529, 332)
+        btnSiguiente.Location = New Point(605, 443)
+        btnSiguiente.Margin = New Padding(3, 4, 3, 4)
         btnSiguiente.Name = "btnSiguiente"
-        btnSiguiente.Size = New Size(69, 25)
+        btnSiguiente.Size = New Size(79, 33)
         btnSiguiente.TabIndex = 53
         btnSiguiente.Text = ">"
         btnSiguiente.UseVisualStyleBackColor = True
         ' 
         ' btnAnterior
         ' 
-        btnAnterior.Location = New Point(454, 332)
+        btnAnterior.Location = New Point(519, 443)
+        btnAnterior.Margin = New Padding(3, 4, 3, 4)
         btnAnterior.Name = "btnAnterior"
-        btnAnterior.Size = New Size(69, 25)
+        btnAnterior.Size = New Size(79, 33)
         btnAnterior.TabIndex = 52
         btnAnterior.Text = "<"
         btnAnterior.UseVisualStyleBackColor = True
         ' 
         ' btnPrimero
         ' 
-        btnPrimero.Location = New Point(454, 363)
+        btnPrimero.Location = New Point(519, 484)
+        btnPrimero.Margin = New Padding(3, 4, 3, 4)
         btnPrimero.Name = "btnPrimero"
-        btnPrimero.Size = New Size(69, 25)
+        btnPrimero.Size = New Size(79, 33)
         btnPrimero.TabIndex = 51
         btnPrimero.Text = "<<"
         btnPrimero.UseVisualStyleBackColor = True
         ' 
         ' btnSalir
         ' 
-        btnSalir.Location = New Point(276, 363)
+        btnSalir.Location = New Point(315, 484)
+        btnSalir.Margin = New Padding(3, 4, 3, 4)
         btnSalir.Name = "btnSalir"
-        btnSalir.Size = New Size(86, 25)
+        btnSalir.Size = New Size(98, 33)
         btnSalir.TabIndex = 50
         btnSalir.Text = "Salir"
         btnSalir.UseVisualStyleBackColor = True
         ' 
         ' btnLimpiar
         ' 
-        btnLimpiar.Location = New Point(276, 332)
+        btnLimpiar.Location = New Point(315, 443)
+        btnLimpiar.Margin = New Padding(3, 4, 3, 4)
         btnLimpiar.Name = "btnLimpiar"
-        btnLimpiar.Size = New Size(86, 25)
+        btnLimpiar.Size = New Size(98, 33)
         btnLimpiar.TabIndex = 49
         btnLimpiar.Text = "Limpiar"
         btnLimpiar.UseVisualStyleBackColor = True
         ' 
         ' btnEliminar
         ' 
-        btnEliminar.Location = New Point(185, 332)
+        btnEliminar.Location = New Point(211, 443)
+        btnEliminar.Margin = New Padding(3, 4, 3, 4)
         btnEliminar.Name = "btnEliminar"
-        btnEliminar.Size = New Size(86, 25)
+        btnEliminar.Size = New Size(98, 33)
         btnEliminar.TabIndex = 48
         btnEliminar.Text = "Eliminar"
         btnEliminar.UseVisualStyleBackColor = True
         ' 
         ' btnEditar
         ' 
-        btnEditar.Location = New Point(93, 332)
+        btnEditar.Location = New Point(106, 443)
+        btnEditar.Margin = New Padding(3, 4, 3, 4)
         btnEditar.Name = "btnEditar"
-        btnEditar.Size = New Size(86, 25)
+        btnEditar.Size = New Size(98, 33)
         btnEditar.TabIndex = 47
         btnEditar.Text = "Editar"
         btnEditar.UseVisualStyleBackColor = True
         ' 
         ' btnGuardar
         ' 
-        btnGuardar.Location = New Point(0, 332)
+        btnGuardar.Location = New Point(0, 443)
+        btnGuardar.Margin = New Padding(3, 4, 3, 4)
         btnGuardar.Name = "btnGuardar"
-        btnGuardar.Size = New Size(86, 25)
+        btnGuardar.Size = New Size(98, 33)
         btnGuardar.TabIndex = 46
         btnGuardar.Text = "Guardar"
         btnGuardar.UseVisualStyleBackColor = True
         ' 
         ' btnRegresar
         ' 
-        btnRegresar.Location = New Point(0, 363)
+        btnRegresar.Location = New Point(0, 484)
+        btnRegresar.Margin = New Padding(3, 4, 3, 4)
         btnRegresar.Name = "btnRegresar"
-        btnRegresar.Size = New Size(270, 25)
+        btnRegresar.Size = New Size(309, 33)
         btnRegresar.TabIndex = 45
         btnRegresar.Text = "Menú principal"
         btnRegresar.UseVisualStyleBackColor = True
         ' 
         ' txtBuscarMedicamento
         ' 
-        txtBuscarMedicamento.Location = New Point(145, 114)
-        txtBuscarMedicamento.Margin = New Padding(3, 2, 3, 2)
+        txtBuscarMedicamento.Location = New Point(175, 152)
         txtBuscarMedicamento.Name = "txtBuscarMedicamento"
-        txtBuscarMedicamento.Size = New Size(143, 23)
+        txtBuscarMedicamento.Size = New Size(163, 27)
         txtBuscarMedicamento.TabIndex = 55
         ' 
         ' Label9
         ' 
         Label9.AutoSize = True
-        Label9.Location = New Point(154, 97)
+        Label9.Location = New Point(176, 129)
         Label9.Name = "Label9"
-        Label9.Size = New Size(119, 15)
+        Label9.Size = New Size(148, 20)
         Label9.TabIndex = 56
         Label9.Text = "Buscar Medicamento"
         ' 
         ' Form10
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1177, 410)
+        ClientSize = New Size(1345, 547)
         Controls.Add(Label9)
         Controls.Add(txtBuscarMedicamento)
         Controls.Add(btnUltimo)
@@ -340,7 +341,6 @@ Partial Class Form10
         Controls.Add(Label2)
         Controls.Add(Label1)
         Controls.Add(txtIdFactura)
-        Margin = New Padding(3, 2, 3, 2)
         Name = "Form10"
         Text = "Form10"
         CType(dgvFacturas, ComponentModel.ISupportInitialize).EndInit()
--- CLINICA MEDICA/CLINICA MEDICA/Form10.vb
+++ CLINICA MEDICA/CLINICA MEDICA/Form10.vb
@@ -3,12 +3,17 @@ Imports System.Globalization
 
 Public Class Form10
 
+    ' --- VARIABLES GLOBALES ---
     Dim tablaFacturas As New DataTable
     Dim posicion As Integer = 0
     Dim cantidadesMedicamentos As New Dictionary(Of Integer, Integer)
 
-    Private Sub Form10_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    ' 🛠️ VARIABLES NUEVAS PARA ARREGLAR TUS FUNCIONES
+    Dim precioFijoConsulta As Decimal = 500D ' Tu precio base
+    Dim actualizandoLista As Boolean = False ' Candado para el buscador
+    Dim tablaMedicamentosGlobal As DataTable ' Memoria de la tabla para buscar sin perder precios
 
+    Private Sub Form10_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         txtIdFactura.ReadOnly = True
         txtMontoTotal.ReadOnly = True
 
@@ -29,13 +34,10 @@ Public Class Form10
         CargarMedicamentosAdquiridos()
         CargarTablaFacturas()
         LimpiarCampos()
-
     End Sub
 
     Private Sub CargarCombos()
-
         Dim dao As New FacturaDAO()
-
         Dim tablaConsultas As DataTable = dao.ObtenerConsultas()
 
         cmbConsulta.DataSource = tablaConsultas
@@ -54,24 +56,20 @@ Public Class Form10
         cmbEstadoPago.Items.Add("Pendiente")
         cmbEstadoPago.Items.Add("Cancelado")
         cmbEstadoPago.SelectedIndex = -1
-
     End Sub
 
     Private Sub CargarMedicamentosAdquiridos()
-
         Dim dao As New FacturaDAO()
-        Dim tablaMedicamentos As DataTable = dao.ObtenerMedicamentos()
+        ' 🛡️ Guardamos la tabla en nuestra variable global para no perderla al buscar
+        tablaMedicamentosGlobal = dao.ObtenerMedicamentos()
 
-        clbMedicamentos.DataSource = tablaMedicamentos
+        clbMedicamentos.DataSource = tablaMedicamentosGlobal
         clbMedicamentos.DisplayMember = "descripcion"
         clbMedicamentos.ValueMember = "id_medicamento"
-
     End Sub
 
     Private Sub CargarTablaFacturas()
-
         Dim dao As New FacturaDAO()
-
         tablaFacturas = dao.ListarFacturas()
         dgvFacturas.DataSource = tablaFacturas
 
@@ -81,11 +79,9 @@ Public Class Form10
         If dgvFacturas.Columns.Contains("fecha_pago") Then dgvFacturas.Columns("fecha_pago").HeaderText = "Fecha de Pago"
         If dgvFacturas.Columns.Contains("metodo_pago") Then dgvFacturas.Columns("metodo_pago").HeaderText = "Método de Pago"
         If dgvFacturas.Columns.Contains("estado_pago") Then dgvFacturas.Columns("estado_pago").HeaderText = "Estado de Pago"
-
     End Sub
 
     Private Function ValidarCampos() As Boolean
-
         If cmbConsulta.SelectedIndex = -1 Then
             MessageBox.Show("Seleccione una consulta.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             cmbConsulta.Focus()
@@ -104,62 +100,60 @@ Public Class Form10
             Return False
         End If
 
-        If txtMontoTotal.Text.Trim() = "" OrElse txtMontoTotal.Text.Trim() = "0.00" Then
-            MessageBox.Show("Seleccione al menos un medicamento para calcular el monto total.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        ' 🛡️ Actualizado: Ahora permite guardar si el monto es al menos el de la consulta base (sin medicamentos)
+        If ObtenerMonto() < precioFijoConsulta Then
+            MessageBox.Show("El monto total no es válido.", "Validación", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             Return False
         End If
 
         Return True
-
     End Function
 
     Private Function ObtenerMonto() As Decimal
-
         Dim monto As Decimal
-
         If Decimal.TryParse(txtMontoTotal.Text.Trim(), NumberStyles.Any, CultureInfo.InvariantCulture, monto) Then
             Return monto
         End If
-
         Decimal.TryParse(txtMontoTotal.Text.Trim(), monto)
         Return monto
-
     End Function
 
     Private Sub LimpiarCampos()
-
         txtIdFactura.Clear()
-        txtMontoTotal.Text = "0.00"
+        ' 🛡️ Al limpiar, regresa al precio base en vez de 0.00
+        txtMontoTotal.Text = precioFijoConsulta.ToString("0.00")
 
         If cmbConsulta.DataSource IsNot Nothing Then cmbConsulta.SelectedIndex = -1
         cmbMetodoPago.SelectedIndex = -1
         cmbEstadoPago.SelectedIndex = -1
 
+        actualizandoLista = True
         For i As Integer = 0 To clbMedicamentos.Items.Count - 1
             clbMedicamentos.SetItemChecked(i, False)
         Next
+        actualizandoLista = False
 
         cantidadesMedicamentos.Clear()
-
+        txtBuscarMedicamento.Clear()
         dtpFechaPago.Value = Date.Now
 
         posicion = 0
         cmbConsulta.Focus()
-
     End Sub
 
-    Private Sub clbMedicamentos_ItemCheck(sender As Object, e As ItemCheckEventArgs) Handles clbMedicamentos.ItemCheck
+    ' --- 🧮 LÓGICA ARREGLADA DEL CÁLCULO Y BUSCADOR ---
 
+    Private Sub clbMedicamentos_ItemCheck(sender As Object, e As ItemCheckEventArgs) Handles clbMedicamentos.ItemCheck
+        ' 🛡️ Evita que salga el InputBox mágicamente al usar el buscador
+        If actualizandoLista Then Exit Sub
         If e.Index < 0 Then Exit Sub
 
+        Dim fila As DataRowView = CType(clbMedicamentos.Items(e.Index), DataRowView)
+        Dim idMedicamento As Integer = CInt(fila("id_medicamento"))
+
         If e.NewValue = CheckState.Checked Then
-
-            Dim fila As DataRowView = CType(clbMedicamentos.Items(e.Index), DataRowView)
-            Dim idMedicamento As Integer = CInt(fila("id_medicamento"))
-            Dim nombreMedicamento As String = fila("nombre_comercial").ToString()
-
+            Dim nombreMedicamento As String = fila("descripcion").ToString()
             Dim cantidadTexto As String = InputBox("Ingrese la cantidad adquirida de: " & nombreMedicamento, "Cantidad", "1")
-
             Dim cantidad As Integer
 
             If Integer.TryParse(cantidadTexto, cantidad) = False OrElse cantidad <= 0 Then
@@ -172,53 +166,67 @@ Public Class Form10
             Else
                 cantidadesMedicamentos.Add(idMedicamento, cantidad)
             End If
-
         ElseIf e.NewValue = CheckState.Unchecked Then
-
-            Dim fila As DataRowView = CType(clbMedicamentos.Items(e.Index), DataRowView)
-            Dim idMedicamento As Integer = CInt(fila("id_medicamento"))
-
             If cantidadesMedicamentos.ContainsKey(idMedicamento) Then
                 cantidadesMedicamentos.Remove(idMedicamento)
             End If
-
         End If
 
         Me.BeginInvoke(New MethodInvoker(AddressOf CalcularTotalMedicamentos))
-
     End Sub
 
     Private Sub CalcularTotalMedicamentos()
+        ' 🛡️ Regresamos tu precio fijo de 500 Lempiras
+        Dim total As Decimal = precioFijoConsulta
 
-        Dim total As Decimal = 0D
+        ' 🛡️ Sumamos usando nuestra memoria inteligente (cantidadesMedicamentos) y buscando el precio en la tabla global
+        For Each kvp In cantidadesMedicamentos
+            Dim idMed As Integer = kvp.Key
+            Dim cant As Integer = kvp.Value
 
-        For Each item As Object In clbMedicamentos.CheckedItems
-
-            Dim fila As DataRowView = CType(item, DataRowView)
-
-            Dim idMedicamento As Integer = CInt(fila("id_medicamento"))
-            Dim precio As Decimal = CDec(fila("precio"))
-
-            Dim cantidad As Integer = 1
-
-            If cantidadesMedicamentos.ContainsKey(idMedicamento) Then
-                cantidad = cantidadesMedicamentos(idMedicamento)
+            ' Buscamos cuánto vale este medicamento
+            Dim filas() As DataRow = tablaMedicamentosGlobal.Select("id_medicamento = " & idMed)
+            If filas.Length > 0 Then
+                Dim precio As Decimal = CDec(filas(0)("precio"))
+                total += (precio * cant)
             End If
-
-            total += precio * cantidad
-
         Next
 
         txtMontoTotal.Text = total.ToString("0.00")
-
     End Sub
 
-    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+    Private Sub txtBuscarMedicamento_TextChanged(sender As Object, e As EventArgs) Handles txtBuscarMedicamento.TextChanged
+        ' 🛡️ Buscador reconectado y compatible con el DataSource de tu compañero
+        If tablaMedicamentosGlobal IsNot Nothing Then
+            actualizandoLista = True
 
+            Dim filtro As String = txtBuscarMedicamento.Text.Trim().Replace("'", "''")
+
+            ' Filtramos la tabla visualmente
+            tablaMedicamentosGlobal.DefaultView.RowFilter = "descripcion LIKE '%" & filtro & "%'"
+
+            ' Devolvemos las palomitas a los medicamentos que siguen visibles y que ya habías seleccionado
+            For i As Integer = 0 To clbMedicamentos.Items.Count - 1
+                Dim fila As DataRowView = CType(clbMedicamentos.Items(i), DataRowView)
+                Dim idMed As Integer = CInt(fila("id_medicamento"))
+
+                If cantidadesMedicamentos.ContainsKey(idMed) Then
+                    clbMedicamentos.SetItemChecked(i, True)
+                Else
+                    clbMedicamentos.SetItemChecked(i, False)
+                End If
+            Next
+
+            actualizandoLista = False
+        End If
+    End Sub
+
+    ' --- BOTONES DE ACCIÓN CRUD ---
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
         If ValidarCampos() = False Then Exit Sub
 
         Dim factura As New Factura()
-
         factura.IdConsulta = CInt(cmbConsulta.SelectedValue)
         factura.MontoTotal = ObtenerMonto()
         factura.FechaPago = dtpFechaPago.Value
@@ -229,14 +237,11 @@ Public Class Form10
         dao.Guardar(factura)
 
         MessageBox.Show("Factura guardada correctamente.", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information)
-
         CargarTablaFacturas()
         LimpiarCampos()
-
     End Sub
 
     Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
-
         If txtIdFactura.Text.Trim() = "" Then
             MessageBox.Show("Seleccione una factura para editar.", "Editar", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             Exit Sub
@@ -245,7 +250,6 @@ Public Class Form10
         If ValidarCampos() = False Then Exit Sub
 
         Dim factura As New Factura()
-
         factura.IdFactura = CInt(txtIdFactura.Text)
         factura.IdConsulta = CInt(cmbConsulta.SelectedValue)
         factura.MontoTotal = ObtenerMonto()
@@ -257,53 +261,42 @@ Public Class Form10
         dao.Editar(factura)
 
         MessageBox.Show("Factura editada correctamente.", "Editar", MessageBoxButtons.OK, MessageBoxIcon.Information)
-
         CargarTablaFacturas()
         LimpiarCampos()
-
     End Sub
 
     Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
-
         If txtIdFactura.Text.Trim() = "" Then
             MessageBox.Show("Seleccione una factura para eliminar.", "Eliminar", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             Exit Sub
         End If
 
-        Dim respuesta As DialogResult
-
-        respuesta = MessageBox.Show("¿Desea eliminar esta factura?", "Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
-
+        Dim respuesta As DialogResult = MessageBox.Show("¿Desea eliminar esta factura?", "Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
         If respuesta = DialogResult.No Then Exit Sub
 
         Dim dao As New FacturaDAO()
         dao.Eliminar(CInt(txtIdFactura.Text))
 
         MessageBox.Show("Factura eliminada correctamente.", "Eliminar", MessageBoxButtons.OK, MessageBoxIcon.Information)
-
         CargarTablaFacturas()
         LimpiarCampos()
-
     End Sub
 
     Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
-
         CargarTablaFacturas()
         LimpiarCampos()
-
     End Sub
 
-    Private Sub dgvFacturas_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvFacturas.CellClick
+    ' --- MANEJO DE TABLA Y NAVEGACIÓN ---
 
+    Private Sub dgvFacturas_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvFacturas.CellClick
         If e.RowIndex >= 0 Then
             posicion = e.RowIndex
             MostrarFactura()
         End If
-
     End Sub
 
     Private Sub MostrarFactura()
-
         If dgvFacturas.Rows.Count = 0 Then Exit Sub
         If posicion < 0 OrElse posicion >= dgvFacturas.Rows.Count Then Exit Sub
 
@@ -327,74 +320,47 @@ Public Class Form10
         Else
             dtpFechaPago.Value = Date.Now
         End If
-
     End Sub
 
     Private Sub btnPrimero_Click(sender As Object, e As EventArgs) Handles btnPrimero.Click
-
         If dgvFacturas.Rows.Count > 0 Then
             posicion = 0
             MostrarFactura()
         End If
-
     End Sub
 
     Private Sub btnAnterior_Click(sender As Object, e As EventArgs) Handles btnAnterior.Click
-
         If dgvFacturas.Rows.Count > 0 Then
-
             If posicion > 0 Then
                 posicion -= 1
                 MostrarFactura()
             Else
                 MessageBox.Show("Ya está en el primer registro.", "Navegación", MessageBoxButtons.OK, MessageBoxIcon.Information)
             End If
-
         End If
-
     End Sub
 
     Private Sub btnSiguiente_Click(sender As Object, e As EventArgs) Handles btnSiguiente.Click
-
         If dgvFacturas.Rows.Count > 0 Then
-
             If posicion < dgvFacturas.Rows.Count - 1 Then
                 posicion += 1
                 MostrarFactura()
             Else
                 MessageBox.Show("Ya está en el último registro.", "Navegación", MessageBoxButtons.OK, MessageBoxIcon.Information)
             End If
-
         End If
-
     End Sub
 
     Private Sub btnUltimo_Click(sender As Object, e As EventArgs) Handles btnUltimo.Click
-
         If dgvFacturas.Rows.Count > 0 Then
             posicion = dgvFacturas.Rows.Count - 1
             MostrarFactura()
         End If
-
     End Sub
 
     Private Sub btnRegresar_Click(sender As Object, e As EventArgs) Handles btnRegresar.Click
-
         Form1.Show()
         Me.Hide()
-
-    End Sub
-
-    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
-
-        Dim respuesta As DialogResult
-
-        respuesta = MessageBox.Show("¿Desea salir del sistema?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
-
-        If respuesta = DialogResult.Yes Then
-            Application.Exit()
-        End If
-
     End Sub
 
 End Class
